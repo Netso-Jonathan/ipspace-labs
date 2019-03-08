c827668550cd2dbda67e4ebae1b611b65c07faeb
jonathan brisson <jonathan@xy1-ubu03.netso.ca>
2019-03-08 11:14:18 -0500

    add local ansible config.cfg

---
+++ gns3/list-device.py
@@ -0,0 +1,11 @@
+import requests
+res = requests.get('http://10.0.8.10:3080/v2/projects/a08ea215-6343-4bd9-b81d-ffe7963dc7f1/nodes', auth=('jonathan', '1J0uriftpn!!'))
+a = res.json()
+
+for node in a:
+ if 'hda_disk_image' in node['properties']:
+  image = node['properties']['hda_disk_image']
+ else: 
+  image = "no Image"
+ console = node['name'] + ', ' + node['console_host'] + ',  ' + str(node['console']) + ', ' + image 
+ print(console)
