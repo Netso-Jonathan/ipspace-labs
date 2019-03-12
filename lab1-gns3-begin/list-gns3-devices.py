8e4bb0efb44c80d657e1cfa9ef3e6547c834ccfc
Jonathan Brisson <jonathan@netso.ca>
2019-03-12 14:22:04 -0400

    Working Python Script to build Ansible inventory

--- lab1-gns3-begin/list-gns3-devices.py
+++ lab1-gns3-begin/list-gns3-devices.py
@@ -1,9 +1,9 @@
 import requests
-import configparser
+# import configparser
 res = requests.get('http://10.0.8.10:3080/v2/projects/a08ea215-6343-4bd9-b81d-ffe7963dc7f1/nodes', auth=('jonathan', '1J0uriftpn!!'))
 a = res.json()
 
-inventory = configparser.ConfigParser(allow_no_value = True)
+# inventory = configparser.ConfigParser(allow_no_value = True)
 
 for node in a:
  if 'hda_disk_image' in node['properties']:
